20d4fcf55926be373454eb02c45e448aabdd02bd
Coder002-239 <90915811+Coder002-239@users.noreply.github.com>
2022-01-12 10:32:41 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "dartMain":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . 8 f f . . . . 
+. . . . . . . . 8 8 8 f . . . . 
+. . . . . . . 8 8 8 8 8 . . . . 
+. . . . . . 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. . . . f 8 8 8 8 . . . . . . . 
+f f f f d d 8 8 . . . . . . . . 
+. . . f d d f . . . . . . . . . 
+. . . f f f . . . . . . . . . . 
+. . f . . f . . . . . . . . . . 
+. f . . . f . . . . . . . . . . 
+f . . . . f . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
