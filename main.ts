be26d35e97d6449a73a8dc0c78946d9f8d7a714e
Coder002-239 <90915811+Coder002-239@users.noreply.github.com>
2022-01-15 13:10:57 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`boost`, function (sprite, location) {
+    timer.throttle("action", 500, function () {
+        myDart.throwDart()
+        music.powerUp.play()
+    })
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     scene.cameraShake(4, 500)
     game.over(false)
@@ -7,11 +13,20 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function createDartMain () {
     myDart = darts.create(assets.image`dartMain`, SpriteKind.Player)
-    myDart.controlWithArrowKeys(true)
     myDart.angle = 45
+    myDart.controlWithArrowKeys(true)
+    myDart.setTrace(true)
+    info.setLife(3)
     scene.cameraFollowSprite(myDart.sprite)
     tiles.placeOnTile(myDart.sprite, tiles.getTileLocation(2, 11))
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`bomb`, function (sprite, location) {
+    scene.cameraShake(4, 500)
+    timer.throttle("action", 500, function () {
+        myDart.angle += -10
+        info.changeLifeBy(-1)
+    })
+})
 let myDart: Dart = null
 scene.setBackgroundImage(assets.image`background`)
 tiles.setTilemap(tilemap`level1`)
