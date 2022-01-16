82aa8868831edaddbc69673f2c0c46d3a0a0f062
Coder002-239 <90915811+Coder002-239@users.noreply.github.com>
2022-01-16 12:55:45 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonPink, function (sprite, location) {
+    music.buzzer.play()
+    game.over(true)
+})
+function initializeLevel2 () {
+    scene.setBackgroundImage(assets.image`background`)
+    tiles.setTilemap(tilemap`level2`)
+    tiles.placeOnTile(myDart.sprite, tiles.getTileLocation(2, 10))
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`boost`, function (sprite, location) {
+    timer.throttle("action", 500, function () {
+        myDart.throwDart()
+        info.changeScoreBy(350)
+    })
+})
+function initializeLevel3 () {
+    scene.setBackgroundImage(assets.image`background`)
+    tiles.setTilemap(tilemap`level3`)
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     scene.cameraShake(4, 500)
     game.over(false)
@@ -7,12 +26,32 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function createDartMain () {
     myDart = darts.create(assets.image`dartMain`, SpriteKind.Player)
-    myDart.controlWithArrowKeys(true)
+    info.setLife(3)
+    info.setScore(0)
     myDart.angle = 45
+    myDart.setTrace()
+    myDart.controlWithArrowKeys(true)
     scene.cameraFollowSprite(myDart.sprite)
+}
+function initializeLevel1 () {
+    scene.setBackgroundImage(assets.image`background`)
+    tiles.setTilemap(tilemap`level1`)
+    createDartMain()
     tiles.placeOnTile(myDart.sprite, tiles.getTileLocation(2, 11))
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonOrange, function (sprite, location) {
+    music.buzzer.play()
+    initializeLevel2()
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`bomb`, function (sprite, location) {
+    timer.throttle("action", 500, function () {
+        info.changeLifeBy(-1)
+        scene.cameraShake(4, 500)
+    })
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.buttonTeal, function (sprite, location) {
+    music.buzzer.play()
+    initializeLevel3()
+})
 let myDart: Dart = null
-scene.setBackgroundImage(assets.image`background`)
-tiles.setTilemap(tilemap`level1`)
-createDartMain()
+initializeLevel1()
